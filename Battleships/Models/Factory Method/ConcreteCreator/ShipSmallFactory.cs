cb6e13539eb3e96ed14c21fcf45075b5d6e5b928
unknown <Gintautas>
2021-12-09 18:41:05 +0200

    FlyWeight

--- Battleships/Models/Factory Method/ConcreteCreator/ShipSmallFactory.cs
+++ Battleships/Models/Factory Method/ConcreteCreator/ShipSmallFactory.cs
@@ -11,21 +11,34 @@ namespace Battleships.Models
 {
     public class ShipSmallFactory : ShipFactory
     {
+        private Dictionary<string, OrangeShip> orange_dict;
+        private Dictionary<string, BlueShip> blue_dict;
+
         private Color _Color;
 
         public ShipSmallFactory()
         {
-
+            orange_dict = new Dictionary<string, OrangeShip>();
+            blue_dict = new Dictionary<string, BlueShip>();
         }
 
         public override OrangeShip GetOrangeShip()
         {
-            return new SmallOrangeShip();
+            string key = "SmallOrange";
+            if (!orange_dict.ContainsKey(key)) {
+                orange_dict[key] = new SmallOrangeShip();
+            }
+            return orange_dict[key];
         }
 
         public override BlueShip GetBlueShip()
         {
-            return new SmallBlueShip();
+            string key = "SmallBlue";
+            if (!blue_dict.ContainsKey(key))
+            {
+                blue_dict[key] = new SmallBlueShip();
+            }
+            return blue_dict[key];
         }
     }
 }
