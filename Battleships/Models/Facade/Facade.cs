0efd51dfe603a99826bbe9769c61138f07e4902b
unknown <Gintautas>
2021-12-09 17:07:26 +0200

    Iterator

--- Battleships/Models/Facade/Facade.cs
+++ Battleships/Models/Facade/Facade.cs
@@ -8,6 +8,7 @@ using Battleships.LevelBuilder;
 using Battleships.Models.Adapter;
 using Battleships.Models.Bridge;
 using Battleships.Models.Command;
+using Battleships.Models.Iterator;
 using WebSocketSharp;
 
 namespace Battleships.Models.Facade
@@ -164,9 +165,10 @@ namespace Battleships.Models.Facade
         public void SpecialAttack()
         {
             List<string> attackships = GetCorrectStrategy(Level, AttackPos, PlayerPos);
-            for (int i = 0; i < attackships.Count; i++)
+            SpecialAttackRepository specialAttackRepository = new SpecialAttackRepository(attackships);
+            for (IIterator i = specialAttackRepository.GetIterator(); i.HasNext();)
             {
-                int index = EnemyPos.GetShipIndex(attackships[i]);
+                int index = EnemyPos.GetShipIndex(i.Next());
                 connections.position_socket.Send($"{index}:{user_id}");
             }
             connections.player_turn.Send($"{user_id}");
---
+++ Battleships/Models/Iterator/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships.Models.Iterator
+{
+    public interface IContainer
+    {
+        IIterator GetIterator();
+    }
+    public interface IIterator
+    {
+        bool HasNext();
+        string Next();
+    }
+
+    class SpecialAttackRepository : IContainer
+    {
+        private List<string> attackships;
+
+        public SpecialAttackRepository(List<string> attackships)
+        {
+            this.attackships = attackships;
+        }
+        public IIterator GetIterator()
+        {
+            return new SpecialAttackIterator(attackships);
+        }
+
+        class SpecialAttackIterator : IIterator
+        {
+            private List<string> attackships;
+            private int index = 0;
+
+            public SpecialAttackIterator(List<string> attackships)
+            {
+                this.attackships = attackships;
+            }
+
+            public bool HasNext()
+            {
+                return index < attackships.Count;
+            }
+
+            public string Next()
+            {
+                if (HasNext())
+                {
+                    return attackships[index++];
+                }
+                return null;
+            }
+        }
+    }
+}
